214ee36853d21d9503d420444be002a623cd2e7a
rCasanueva <rolacasanueva@gmail.com>
2017-06-23 20:01:24 -0400

    codigos 1

--- Tarea 3/main.py
+++ Tarea 3/main.py
@@ -4,21 +4,22 @@
 #            Libraries
 #--------------------------------------------
 from pyspark import SparkContext, SparkConf
+from pyspark.mllib.recommendation import ALS, Rating
+from math import sqrt
 
 
 #--------------------------------------------
 #            spark_init
 #--------------------------------------------
 #   FUNCTION_IN_PARAMETERS_DEFINITION
-#   name:  String name for environ [not required]
+#	None
 #
 #   FUNCTION_OUT_PARAMETERS_DEFINITION
 #   out:    SparkContext object
 #   FUNCTION_CODING
-def spark_init(name='Default Name'):
-	conf = SparkConf().setAppName(name)
-	sc = SparkContext(conf=conf)
-    return sc
+def spark_init():
+	sc = SparkContext()
+	return sc
 #   FUNCTION_EXPLANATION
 #   Creates a new JVM, if non is already created,
 #	and return the context object to work with
@@ -29,49 +30,77 @@ def spark_init(name='Default Name'):
 #            read_datasets
 #--------------------------------------------
 #   FUNCTION_IN_PARAMETERS_DEFINITION
-#   mode:   one letter string which makes read
-#			of the ratings.dat file or the training
-#			and tests ones.[Not required]
+#	sc:		spark context object
 #
 #   FUNCTION_OUT_PARAMETERS_DEFINITION
 #   out:    Tuple of lists of tuples
 #   FUNCTION_CODING
-def read_datasets(mode=None):
+def read_datasets(sc, mode=None):
 	try:
-		file_movies = open("DataSets/movies.dat", "r")
-		file_tags = open("DataSets/tags.dat", "r")
+		file_movies = sc.textFile("DataSets/movies.dat")
+		file_tags = sc.textFile("DataSets/tags.dat")
 	except:
 		print "ERROR: Not all Files were found."
 		return -1
-
-	movies = list()
-    for line in file_movies:
-		aux = list()
-		line = line.strip().split('::')
-		aux.append(line[0]).append(line[1]).append(line[2].split('|'))
-		movies.append(tuple(aux))
-	file_movies.close()
-
-	tags = list()
-    for line in file_tags:
-		line = line.strip().split('::')
-		tags.append(tuple(line))
-	file_tags.close()
-
-	if not mode:
-		try:
-			file_ratings = open("DataSets/ratings.dat", "r")
-		except:
-			print "ERROR: Not all Files were found."
-			return -1
-		ratings = list()
-	    for line in file_ratings:
-			line = line.strip().split('::')
-			ratings.append(tuple(line))
-		file_ratings.close()
-	return (movies, tags, ratings)
+	movies = file_movies.map(lambda line: line.split("::"))
+	tags = file_movies.map(lambda line: line.split("::"))
+	return (movies, tags)
 #   FUNCTION_EXPLANATION
-#   Creates a new JVM, if non is already created,
-#	and return the context object to work with
-#	spark utilities
+#   Open Datasets and returns them into python variables
 #--------------------------------------------
+
+def read_file(filename):
+	file_ = open(filename)
+	data = list()
+	for line in file_:
+		line = line.strip().split("::")
+		data.append((int(line[0]), int(line[1]), float(line[2])))
+	return data
+
+def read_ratings(sc):
+	train1 = sc.parallelize(read_file("DataSets/SplitData/r1.train"))
+	train2 = sc.parallelize(read_file("DataSets/SplitData/r2.train"))
+	train3 = sc.parallelize(read_file("DataSets/SplitData/r3.train"))
+	train4 = sc.parallelize(read_file("DataSets/SplitData/r4.train"))
+	train5 = sc.parallelize(read_file("DataSets/SplitData/r5.train"))
+	test1 = sc.parallelize(read_file("DataSets/SplitData/r1.test"))
+	test2 = sc.parallelize(read_file("DataSets/SplitData/r2.test"))
+	test3 = sc.parallelize(read_file("DataSets/SplitData/r3.test"))
+	test4 = sc.parallelize(read_file("DataSets/SplitData/r4.test"))
+	test5 = sc.parallelize(read_file("DataSets/SplitData/r5.test"))
+	train = (train1, train2, train3, train4, train5)
+	test = (test1, test2, test3, test4, test5)
+	ratings = (train, test)
+	return ratings
+
+
+def system_train(sc, ratings, rank, lambda_=0.01):
+#	ratings:	RDD of Rating or (userID, productID, rating) tuple.
+#	rank:		Rank of the feature matrices computed (number of features).
+#	lambda:		Regularization parameter. (default: 0.01)
+	model = ALS.train(ratings, rank=rank, lambda_=lambda_, seed=0)
+	return model
+
+
+if __name__ ==  "__main__":
+	sc = spark_init()
+	movies, tags = read_datasets(sc)
+	ratings = read_ratings(sc)
+	train, test = ratings
+	tr1, tr2, tr3, tr4, tr5 = train
+	ts1, ts2, ts3, ts4, ts5 = test
+
+	ranks = [1, 5, 10, 50, 100]
+	lambdas = [0.01, 0.02]
+
+	for rank in ranks:
+		for lambda_ in lambdas:
+			model = system_train(sc, tr1, rank, lambda_)
+			predict = model.predictAll(ts1.map(lambda gp: (gp[0], gp[1]))).map(lambda res: (res[2]))
+			validation = ts1.map(lambda real: (real[2]))
+			union = validation.join(predict)
+			RMSE = sqrt(union.map(lambda row: (row[0] - row[1])**2).mean())
+
+			results = open("Results/results.dat", "a")
+			results.write('rank:'+str(rank)+' lambda:'+str(lambda_)+' RMSE:'+str(RMSE))
+	
